e46e91796f170e5ede748906db89e0f9ac15d8e0
rojin254 <rojinraju7@gmail.com>
2023-07-19 21:38:07 +0530

    tabs and half conntent done

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsComponent } from './tabs/tabs.component';
+
+const routes: Routes = [
+  {path:'',component:TabsComponent}
+
+];
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,45 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppComponent } from './app.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+
+import {MatGridListModule} from '@angular/material/grid-list';
+
+import {MatInputModule} from '@angular/material/input';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatButtonModule} from '@angular/material/button';
+import {MatTabsModule} from '@angular/material/tabs';
+import {MatStepperModule} from '@angular/material/stepper';
+import { TabsComponent } from './tabs/tabs.component';
+import { AboutComponent } from './about/about.component';
+import { BackgroundComponent } from './background/background.component';
+import { SkillsComponent } from './skills/skills.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabsComponent,
+    AboutComponent,
+    BackgroundComponent,
+    SkillsComponent
+    
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    MatButtonModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatStepperModule,
+    MatTabsModule,
+    MatGridListModule
+    
   ],
   providers: [],
   bootstrap: [AppComponent]
